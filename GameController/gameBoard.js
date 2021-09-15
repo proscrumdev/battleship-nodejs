de8546cfc136c4d19ea47878f038a02b56539535
Andrew Gauger <andrew.gauger@fountain.com>
2021-09-15 13:19:05 -0700

    move board to gameboard

--- GameController/gameBoard.js
+++ GameController/gameBoard.js
@@ -9,6 +9,7 @@ class GameBoard {
     'G': 7,
     'H': 8
   }
+  ALPHABET = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
   EMPTY = ' ';
   OCCUPIED = 'X';
@@ -37,6 +38,61 @@ class GameBoard {
 
   render() {
     console.table(this.board);
+  }
+
+  shipsEndingCoordinate(pos,len) {
+    const row = pos.charAt(0)
+    const col = parseInt(pos.charAt(1));
+    var options = []
+
+    // Make sure the opening move is valid
+    if (this.board[row][col] === ' ') {
+      options.push(this.checkLeft(row,col,len))
+      options.push(this.checkRight(row,col,len))
+      options.push(this.checkDown(row,col,len))
+      options.push(this.checkUp(row,col,len))
+    }
+    return options.filter( el => el !== '')
+  }
+  checkLeft(row,col,len) {
+    const start_at = col
+    if(col-len < 1)
+      return '';
+    for(col;col > start_at-len;col--) {
+      if(this.board[row][col] !== ' ')
+        return '';
+    }
+    return(`${row}${col}`);
+  }
+  checkRight(row,col,len) {
+    const start_at = col
+    if(col+len > 8)
+      return '';
+    for(col;col < len+start_at;col++) {
+      if(this.board[row][col] !== ' ')
+        return '';
+    }
+    return(`${row}${col}`);
+  }
+  checkUp(row,col,len) {
+    var row_as_number = this.ALPHABET.indexOf(row)
+    if(row_as_number - len < 1)
+      return '';
+    for(row_as_number;row_as_number > this.ALPHABET.indexOf(row)-len;row_as_number--) {
+      if(this.board[this.ALPHABET[row_as_number]][col] !== ' ')
+        return '';
+    }
+    return(this.ALPHABET[row_as_number]+col)
+  }
+  checkDown(row,col,len) {
+    var row_as_number = this.ALPHABET.indexOf(row)
+    if(row_as_number + len > 8)
+      return '';
+    for(row_as_number;row_as_number < this.ALPHABET.indexOf(row)+len;row_as_number++) {
+      if(this.board[this.ALPHABET[row_as_number]][col] !== ' ')
+        return '';
+    }
+    return(this.ALPHABET[row_as_number] + col)
   }
 }
 
