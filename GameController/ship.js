74d7961357dd8a73202be99181e89a8ad6f3f43f
William Wallace <william.wallace.ww1@roche.com>
2022-04-19 12:01:09 -0700

    Test for continuity

--- GameController/ship.js
+++ GameController/ship.js
@@ -9,6 +9,34 @@ class Ship {
     addPosition(position) {
         this.positions.push(position);
     }
+  
+    isContinous() {
+      var counter = 0 
+      this.positions.forEach(_shipPosition => {
+        if (this.hasNeighbour(_shipPosition)) {
+          counter++
+        }  
+      });
+      return this.positions.length == counter
+    }
+  
+    hasNeighbour(oldPosition) {   
+      const upPos = this.includes(oldPosition.column.value, oldPosition.row - 1)
+      const downPos = this.includes(oldPosition.column.value, oldPosition.row + 1)
+      const leftPos = this.includes(oldPosition.column.value - 1, oldPosition.row)
+      const rightPos = this.includes(oldPosition.column.value + 1, oldPosition.row)
+      return upPos || downPos || leftPos || rightPos
+    }
+  
+    includes(x,y) {
+      var retVal = false
+      this.positions.forEach(_shipPosition => {
+          if(y == _shipPosition.row && x == _shipPosition.column.value) {
+            retVal = true
+          }
+      });
+      return retVal
+    }
 }
 
 module.exports = Ship;
