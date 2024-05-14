ffa088c792e1fcd6ba5aba6a9f35714bbec48884
Bret Kruse <bret.kruse@epicgames.com>
2024-05-14 11:59:55 -0400

    Adds logic for when game is over

--- GameController/gameController.js
+++ GameController/gameController.js
@@ -1,35 +1,45 @@
+const Battleship = require("../battleship.js");
+
 class GameController {
-    static InitializeShips() {
-        var colors = require("cli-color");
-        const Ship = require("./ship.js");
-        var ships = [
-            new Ship("Aircraft Carrier", 5, colors.CadetBlue),
-            new Ship("Battleship", 4, colors.Red),
-            new Ship("Submarine", 3, colors.Chartreuse),
-            new Ship("Destroyer", 3, colors.Yellow),
-            new Ship("Patrol Boat", 2, colors.Orange)
-        ];
-        return ships;
-    }
+  static InitializeShips() {
+    var colors = require("cli-color");
+    const Ship = require("./ship.js");
+    var ships = [
+      new Ship("Aircraft Carrier", 5, colors.CadetBlue),
+      new Ship("Battleship", 4, colors.Red),
+      new Ship("Submarine", 3, colors.Chartreuse),
+      new Ship("Destroyer", 3, colors.Yellow),
+      new Ship("Patrol Boat", 2, colors.Orange),
+    ];
+    return ships;
+  }
 
-    static CheckIsHit(ships, shot) {
-        if (shot == undefined)
-            throw "The shooting position is not defined";
-        if (ships == undefined)
-            throw "No ships defined";
-        var returnvalue = false;
-        ships.forEach(function (ship) {
-            ship.positions.forEach(position => {
-                if (position.row == shot.row && position.column == shot.column)
-                    returnvalue = true;
-            });
-        });
-        return returnvalue;
-    }
+  static CheckIsHit(ships, shot) {
+    console.log("CHECK IS HIT");
+    if (shot == undefined) throw "The shooting position is not defined";
+    if (ships == undefined) throw "No ships defined";
+    var returnvalue = false;
+    ships.forEach(function (ship) {
+      ship.positions.forEach((position) => {
+        if (position.row == shot.row && position.column == shot.column) {
+          ship.addHit(`${shot.column}${shot.row}`);
+          returnvalue = true;
+        }
+      });
+    });
 
-    static isShipValid(ship) {
-        return ship.positions.length == ship.size;
-    }
+    return returnvalue;
+  }
+
+  static CheckIsFleetSunk(ships) {
+    if (ships == undefined) throw "No ships defined";
+    var remainingShips = ships.filter((ship) => !ship.isSunk());
+    return remainingShips.length === 0;
+  }
+
+  static isShipValid(ship) {
+    return ship.positions.length == ship.size;
+  }
 }
 
 module.exports = GameController;
